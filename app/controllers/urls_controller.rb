e2f97e509ea5214c8f8c056c27a582fa58f5c77c
Enricomaine <Enricomaine@gmail.com>
2025-07-02 18:29:47 -0300

    Adds Urls_controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,2 +1,34 @@
 class UrlsController < ApplicationController
+  def create 
+    url = Url.new(url_params)
+    if url.save 
+      render json: { short_url: short_url(url.short), original: url.original }, status: :created
+    else 
+      render json: {errors: url.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show 
+    url = Url.find(params[:id])
+    render json: { short_url: short_url(url.short), original: url.original }
+  end
+
+  def redirect 
+    url = Url.find_by(short: params[:short])
+    if url 
+      redirect_to url.original, allow_other_host: true 
+    else
+      render json: { error: "URL not found" }, status: :not_found
+    end
+  end
+
+  private 
+
+  def url_params 
+    params.require(:url).permit(:original)
+  end
+
+  def short_url(code)
+    "#{request.base_url}/#{code}"
+  end
 end
--- db/migrate/20250702192352_create_urls.rb
+++ db/migrate/20250702192352_create_urls.rb
@@ -1,11 +1,11 @@
 class CreateUrls < ActiveRecord::Migration[8.0]
   def change
     create_table :urls do |t|
-      t.string :original
-      t.string :short
+      t.string :original, null: false
+      t.string :short, null: false
 
       t.timestamps
     end
-    add_index :urls, :short
+    add_index :urls, :short, unique: true
   end
 end
