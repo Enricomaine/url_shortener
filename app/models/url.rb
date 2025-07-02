3b1fecc97af2efbf255a7024352b9a635c2ed7ab
Enricomaine <Enricomaine@gmail.com>
2025-07-02 16:35:25 -0300

    adds url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,15 @@
 class Url < ApplicationRecord
-  validates
+  validates :original, presence: true, format: URI::regexp(%w[http https])
+  validates :short, uniqueness: true 
+
+  before_create :generate_short_code
+
+  private 
+
+  def generate_short_code
+    self.short = loop do 
+      code = SecureRandom.alphanumeric(6)
+      break code unless Url.exists?(short: code)
+    end
+  end
 end
